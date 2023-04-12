256774eb659fbc71b046b5d6857385aae914d837
TiredSnorlax <weide.sg05@gmail.com>
2023-04-12 19:12:05 +0800

    changed env

--- src/lib/db/setup.ts
+++ src/lib/db/setup.ts
@@ -1,9 +1,10 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { PUBLIC_API_KEY } from "$env/static/public";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDBazW8ezdl457-cmYQIPu_F2nzcKu8wVE",
+  apiKey: PUBLIC_API_KEY,
   authDomain: "study-cards-9fe56.firebaseapp.com",
   projectId: "study-cards-9fe56",
   storageBucket: "study-cards-9fe56.appspot.com",
@@ -11,6 +12,7 @@ const firebaseConfig = {
   appId: "1:587506418443:web:150804a8b90ab42915580e",
   measurementId: "G-VCZ90KEQ2N"
 };
+
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app)
